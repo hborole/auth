0c3d0716ed0f966512e1bf58463da144ce91e319
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-17 10:30:34 +0530

    Completed Error Handling Middleware

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -16,16 +18,17 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     console.log('Creating a user...');
+    throw new DatabaseConnectionError();
 
     // Create a new user
 
-    return res.status(201).json({ message: 'User created!' });
+    res.status(201).json({ message: 'User created!' });
   }
 );
 
