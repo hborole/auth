3330a0e668ef4092f9d69cb141639e3973fd5a69
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-18 13:00:17 +0530

    Sign in route complete

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
@@ -11,8 +16,39 @@ router.post(
     body('password').trim().notEmpty().withMessage('Password must be valid'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate a JWT
+    const userJWT = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJWT,
+    };
+
+    return res.status(200).send(existingUser);
   }
 );
 
