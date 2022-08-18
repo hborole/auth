83c41ea0055581bf3f134d28152985466cd6d94a
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-18 10:09:28 +0530

    Sign up user with hased password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../../services/password';
 
 // An interface that describes the properties that are required to create a new User
 // Helps TS understand properties on User
@@ -21,6 +22,14 @@ interface UserModel extends mongoose.Model<UserDoc> {
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 // This is how to add a function to a model in typescript
