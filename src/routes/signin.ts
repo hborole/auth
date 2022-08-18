f41725ead69cc1b1d09f626aa7569b21a864769b
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-18 12:51:13 +0530

    Create validate request middleware

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password must be valid'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signinRouter };
