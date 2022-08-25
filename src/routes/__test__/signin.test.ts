1fd76908abf398ac65da019c918665d07229e366
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-25 16:33:01 +0530

    Attempt fix for test

--- src/routes/__test__/signin.test.ts
+++ src/routes/__test__/signin.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('fails when an email that does not exist is supplied', async () => {
-  await request(app)
+  return request(app)
     .post('/api/users/signin')
     .send({
       email: 'test@test.com',
